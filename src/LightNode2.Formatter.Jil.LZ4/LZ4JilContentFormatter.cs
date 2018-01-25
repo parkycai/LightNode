dc8975089f137bd46e0d8242fdb1cddfa3b8e2e4
Ikiru Yoshizaki <cherishacquire@outlook.com>
2018-01-26 03:10:21 +0900

    Add(LightNode2.Formatter.Jil.LZ4) NETStandard ormatter Jil LZ4
    Change lz4.net to zjil.lz4.core to support netstandard 1.6

---
+++ src/LightNode2.Formatter.Jil.LZ4/LZ4JilContentFormatter.cs
@@ -0,0 +1,72 @@
+﻿using Jil;
+using LightNode2.Core;
+using LZ4;
+using System;
+using System.IO;
+using System.Text;
+
+namespace LightNode2.Formatter
+{
+    public class LZ4JilContentFormatter : ContentFormatterBase
+    {
+        readonly Options options;
+
+        public override string ContentEncoding
+        {
+            get
+            {
+                return "lz4";
+            }
+        }
+
+        public LZ4JilContentFormatter(string mediaType = "application/json", string ext = "json")
+            : this(null, new UTF8Encoding(false), mediaType, ext)
+        {
+
+        }
+        public LZ4JilContentFormatter(Options options, string mediaType = "application/json", string ext = "json")
+            : this(options, new UTF8Encoding(false), mediaType, ext)
+        {
+
+        }
+
+        public LZ4JilContentFormatter(Encoding encoding, string mediaType = "application/json", string ext = "json")
+            : this(null, encoding, mediaType, ext)
+        {
+
+        }
+
+        public LZ4JilContentFormatter(Options options, Encoding encoding, string mediaType = "application/json", string ext = "json")
+            : base(mediaType, ext, encoding)
+        {
+            this.options = options;
+        }
+
+        public override void Serialize(System.IO.Stream stream, object obj)
+        {
+            // default LZ4Stream buffer size is 1MB but it's too large on WebService serialization
+            using (var lz4 = new LZ4Stream(stream, LZ4StreamMode.Compress, blockSize: 1024 * 64))
+            using (var sw = new StreamWriter(lz4, Encoding ?? new UTF8Encoding(false)))
+            {
+                JSON.Serialize(obj, sw, options);
+            }
+        }
+
+        public override object Deserialize(Type type, System.IO.Stream stream)
+        {
+            using (var gzip = new LZ4Stream(stream, LZ4StreamMode.Decompress, blockSize: 1024 * 64))
+            using (var sr = new StreamReader(gzip, new UTF8Encoding(false)))
+            {
+                return JSON.Deserialize(sr, type, options);
+            }
+        }
+    }
+
+    public class LZ4JilContentFormatterFactory : IContentFormatterFactory
+    {
+        public IContentFormatter CreateFormatter()
+        {
+            return new LZ4JilContentFormatter();
+        }
+    }
+}
